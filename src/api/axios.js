5c38463921c406b31460d41901e9b46fa7171b72
alfredfullstack2024 <alfredfullstack@gmail.com>
2025-06-12 09:00:58 -0500

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -38,10 +38,10 @@ api.interceptors.response.use(
 
 export const obtenerClases = (config) => api.get("/clases", config);
 export const eliminarClase = (id, config) => api.delete(`/clases/${id}`, config);
-export const obtenerEntrenadores = (config) => api.get("/entrenadores", config);
-export const obtenerEntrenadorPorId = (id, config) => api.get(`/entrenadores/${id}`, config);
-export const editarEntrenador = (id, data, config) => api.put(`/entrenadores/${id}`, data, config);
-export const crearEntrenador = (data, config) => api.post("/entrenadores", data, config); // Añadido para crear
+export const obtenerEntrenadores = (config) => api.get("/entrenadors", config);
+export const obtenerEntrenadorPorId = (id, config) => api.get(`/entrenadors/${id}`, config);
+export const editarEntrenador = (id, data, config) => api.put(`/entrenadors/${id}`, data, config);
+export const crearEntrenador = (data, config) => api.post("/entrenadors", data, config); // Añadido para crear
 export const obtenerClientes = (config) => api.get("/clientes", config);
 export const consultarClientePorCedula = (numeroIdentificacion, config) => api.get(`/clientes/consultar/${numeroIdentificacion}`, config);
 export const obtenerClientePorId = (id, config) => api.get(`/clientes/${id}`, config);
@@ -51,7 +51,7 @@ export const eliminarCliente = (id, config) => api.delete(`/clientes/${id}`, con
 export const obtenerClientesActivos = async (config = {}) => {
   try {
     const response = await api.get("/clientes/activos", config);
-    return response;
+      return response;
   } catch (error) {
     throw error;
   }
@@ -77,7 +77,7 @@ export const obtenerTransaccionPorId = (id, config) => api.get(`/contabilidad/${
 export const crearTransaccion = (data, config) => api.post("/contabilidad", data, config);
 export const editarTransaccion = (id, data, config) => api.put(`/contabilidad/${id}`, data, config);
 export const eliminarTransaccion = (id, config) => api.delete(`/contabilidad/${id}`, config);
-export const eliminarEntrenador = (id, config) => api.delete(`/entrenadores/${id}`, config);
+export const eliminarEntrenador = (id, config) => api.delete(`/entrenadors/${id}`, config);
 export const obtenerRutinas = (config) => api.get("/rutinas", config);
 export const crearRutina = (data, config) => api.post("/rutinas", data, config);
 export const editarRutina = (id, data, config) => api.put(`/rutinas/${id}`, data, config);
