2d8cfa361bd4decbc4817e3cb73d9f3a0156ed83
alfredfullstack2024 <alfredfullstack@gmail.com>
2025-06-16 13:50:18 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
+// src/index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { AuthProvider } from "./context/AuthContext"; // Importa AuthProvider
+import { BrowserRouter } from "react-router-dom"; // Importa BrowserRouter
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
