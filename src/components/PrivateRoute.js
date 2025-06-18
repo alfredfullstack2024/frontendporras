c11aa4e15dbaa848a0e07bbee3bafc9a3f2bb317
alfredfullstack2024 <alfredfullstack@gmail.com>
2025-06-18 13:26:09 -0500

    Update PrivateRoute.js

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -3,9 +3,8 @@ import { Navigate, Outlet, useLocation } from "react-router-dom";
 
 const PrivateRoute = () => {
   const location = useLocation();
-
-  // Verificar token para rutas protegidas
   const token = localStorage.getItem("token");
+
   if (!token) {
     return <Navigate to="/login" replace />;
   }
