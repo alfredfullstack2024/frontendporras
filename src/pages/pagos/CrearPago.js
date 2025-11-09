f3da7966f89cfd116da27e2fa8248fac6ae77c74
alfredfullstack2024 <alfredfullstack@gmail.com>
2025-11-09 13:32:16 -0500

    Refactor CrearPago component for better readability

--- src/pages/pagos/CrearPago.js
+++ src/pages/pagos/CrearPago.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+  import React, { useState, useEffect } from "react";
 import { Form, Button, Alert, Card } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import { obtenerClientes, obtenerProductos, crearPago } from "../../api/axios";
